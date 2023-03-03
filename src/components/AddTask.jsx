b9f50567c8f4176d1b06c13a4647fd45222f877e
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-03-03 19:33:06 +0100

    finitoDay1

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -2,33 +2,26 @@ const AddTask = () => {
   return (
     <div>
       <form className="form-group">
-        <div className="form-group text-left">
+        <div className="form-group text-left mt-3">
           <label for="addTask">Task</label>
-          <select
-            className="form-control"
-            id="addTask"
-          >
+          <select className="form-control" id="addTask">
             <option hidden>Add Task</option>
             <option>Work Out</option>
             <option>Study Project</option>
             <option>Cook Food</option>
           </select>
         </div>
-        <div className="form-group text-left">
-          <label for="dayTime">
-            Day & Time
-          </label>
-          <select
-            className="form-control"
-            id="dayTime"
-          >
+        <div className="form-group text-left mt-3">
+          <label for="dayTime">Day & Time</label>
+          <select className="form-control" id="dayTime">
             <option hidden>Add Day & Time</option>
             <option>Mar 5th at 15:30</option>
             <option>Mar 7th at 21:30</option>
           </select>
         </div>
-
-        <button type="submit" className="btn btn-primary mt-2 w-100">Save Task</button>
+        <button type="submit" className="btn btn-info mt-3 w-100">
+          Save Task
+        </button>
       </form>
     </div>
   );
